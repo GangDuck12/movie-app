8026e6036118436e00b10abb98b1c504f9f3fef2
gangduck <GangDuck12@gmail.com>
2020-08-12 15:25:14 +0900

    Movie.js DEEFAULT_ movie.Poster modify

--- src/Movie.js
+++ src/Movie.js
@@ -10,7 +10,7 @@ const DEFAULT_PLACEHOLDER_IMAGE =
 
 const Movie = ({movie}) => {
     const poster = 
-        movie.Poster === "M/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+        movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
     return(
         <div className="movie">
             <h2>{movie.Title}</h2>
